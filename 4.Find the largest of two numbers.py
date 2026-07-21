3357015fe621ceda04b299ea4975bde91901f123
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-07-21 20:00:26 +0530

    first commit

---
+++ 4.Find the largest of two numbers.py
@@ -0,0 +1,8 @@
+# Find the largest of two numbers.
+
+int1=int(input("Enter A first number="))
+int2=int(input("Enter A Second Number="))
+if (int1 > int2):
+    print(f"{int1} is the greatest")
+else:
+    print(f"{int2} is the greatest")
