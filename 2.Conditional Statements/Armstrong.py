c6c54d25de23b50c6e44783df2fbf15869baa59e
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-07-21 19:34:48 +0530

    first commit

--- 2.Conditional Statements/Armstrong.py
+++ 2.Conditional Statements/Armstrong.py
@@ -1,2 +1,14 @@
+def two(nums, target):
+    n_map = {}
+
+    for i, num in enumerate(nums):
+        indi = target - num
+        if indi in n_map:
+            return [n_map[indi], i]
+            n_map[num] = i
 
 
+print(two([2, 7, 11, 15],9))
+# print(two([3,2,4],6))
+# print(two([3,3],6))
+print(two([2, 7, 11, 15], 9))
---
+++ 3.Swapping Two Numbers.py
@@ -0,0 +1,8 @@
+# Swap two numbers without using a third variable.
+
+
+a=int(input("Enter a Number:"))
+b=int(input("Enter a Second Number:"))
+print("Before Swapping",a,b)
+a,b =b,a
+print("After Swapping",a,b)
