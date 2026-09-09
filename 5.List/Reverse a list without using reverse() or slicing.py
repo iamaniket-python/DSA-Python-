ac14a327378a426e8fd29b241c6bf1adbb6e7614
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-09-09 19:56:54 +0530

    python

--- 5.List/Reverse a list without using reverse() or slicing.py
+++ 5.List/Reverse a list without using reverse() or slicing.py
@@ -1,1 +1,9 @@
 # Reverse a list without using reverse() or slicing
+
+numbers=[34,32,45,78]
+reverse_list=[]
+
+for i in range(len(numbers) -1,-1,-1):
+    reverse_list.append(numbers[i])
+
+print(reverse_list)
